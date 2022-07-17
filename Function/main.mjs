59c298985fd6c876279515ac6a8782308a59a762
Luisa Fernanda Barragan Avila <luisaavila304@gmail.com>
2022-07-16 21:37:36 -0500

    Control de acceso a archivos

--- Function/main.mjs
+++ Function/main.mjs
@@ -1,5 +1,5 @@
-import { userDatabase } from "./Archive/ingresoUser.mjs";
-import { viewLogin } from "./View/viewLogin.mjs";
+import { userDatabase } from "../Archive/ingresoUser.mjs";
+import { viewLogin } from "../View/viewLogin.mjs";
 
 userDatabase();
 viewLogin("block");
